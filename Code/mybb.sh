cdfd8d9ac376b3a6884f07117762835405284261
Jamsheer Kandoth <jamsheer@yougotagift.com>
2017-01-22 15:02:18 +0400

    MyBB-Cloudformation Template

--- Code/mybb.sh
+++ Code/mybb.sh
@@ -9,8 +9,26 @@ WEB="/var/www/html"
 SRC="/tmp/mybb/Code"
 
 cp "$SRC"/* "$WEB"/
-cd "$WEB"
-mv inc/config.default.php inc/config.php
-chmod -R 0777 cache uploads inc/settings.php inc/config.php
-chmod 777 admin/backups/
 
+sed -e "s/MYBB_ADMINEMAIL/${MYBB_ADMINEMAIL}/g" \
+    -e "s/MYBB_DOMAINNAME/${MYBB_DOMAINNAME}/g" \
+    "${SRC}/settings.php" > "${WEB}/inc/settings.php"
+
+sed -e "s/MYBB_DBPASSWORD/${MYBB_DBPASSWORD}/g" \
+    -e "s/MYBB_SERVER/${MYBB_SERVER}/g" \
+    "${SRC}/config.php" > "${WEB}/inc/config.php"
+
+# Initialize database.
+sed -e "s/MYBB_ADMINEMAIL/${MYBB_ADMINEMAIL}/g" \
+    -e "s/MYBB_DOMAINNAME/${MYBB_DOMAINNAME}/g" \
+    "${SRC}/mybb.sql" | mysql \
+    --user="mybb" \
+    --password="$MYBB_DBPASSWORD" \
+    --host="$MYBB_SERVER" \
+    --database="mybb"
+
+cd "$WEB"
+chmod 555 inc/config.php inc/settings.php
+chmod 555 inc/languages/english/*.php inc/languages/english/admin/*.php
+chmod 777 cache/ cache/themes/ uploads/ uploads/avatars/ admin/backups/
+rm -rf install
